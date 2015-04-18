92ee3c07b5a0a02296a94d95636a7c5fba553c0c
koshigoe <koshigoeb@gmail.com>
2015-04-18 11:31:07 +0900

    Add `config.error_logger`.

--- lib/rails_error_logger/railtie.rb
+++ lib/rails_error_logger/railtie.rb
@@ -3,9 +3,13 @@ require 'rails_error_logger/logger'
 
 module RailsErrorLogger
   class Railtie < Rails::Railtie
-    initializer :add_bugsnag_logger, before: :load_config_initializers do
-      error_logger = RailsErrorLogger::Logger.new(:bugsnag)
-      error_logger.level = Logger::WARN
+    config.error_logger = ActiveSupport::OrderedOptions.new
+    config.error_logger.log_device = :bugsnag
+    config.error_logger.log_level = Logger::ERROR
+
+    initializer :add_error_logger, before: :load_config_initializers do
+      error_logger = RailsErrorLogger::Logger.new(config.error_logger.log_device)
+      error_logger.level = config.error_logger.log_level
       Rails.logger.extend ActiveSupport::Logger.broadcast(error_logger)
     end
   end
